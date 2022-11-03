5cb5edb08aba8f52053635428b2a20b37242f44b
sdin <sdin@LAPTOP-DINESH>
2022-11-03 10:30:34 +0100

    Response types added

--- Hotel_HotelAPI/Controllers/HotelAPIController.cs
+++ Hotel_HotelAPI/Controllers/HotelAPIController.cs
@@ -9,12 +9,16 @@ namespace Hotel_HotelAPI.Controllers
     public class HotelAPIController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<HotelDTO>> GetHotels()
         {
             return Ok(HotelStore.hotelList);
         }
 
         [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<HotelDTO> GetHotel(int id)
         {
             if(id==0)
