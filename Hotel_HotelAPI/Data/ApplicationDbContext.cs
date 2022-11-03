7ebbfeec455f724d3b62757e9e45121140aeb4d6
sdin <sdin@LAPTOP-DINESH>
2022-11-03 16:23:25 +0100

    Create hotel table with successful connection

--- Hotel_HotelAPI/Data/ApplicationDbContext.cs
+++ Hotel_HotelAPI/Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace Hotel_HotelAPI.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+
+        }
+  
         public DbSet<Hotel> Hotels { get; set; }
     }
 }
