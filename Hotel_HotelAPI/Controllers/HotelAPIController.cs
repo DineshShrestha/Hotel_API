123ab48566bd8303877f6256a816e28cd8d8ae7f
sdin <sdin@LAPTOP-DINESH>
2022-11-03 09:49:13 +0100

    first endpoint HTTPGet

--- Hotel_HotelAPI/Controllers/HotelAPIController.cs
+++ Hotel_HotelAPI/Controllers/HotelAPIController.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel_HotelAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 namespace Hotel_HotelAPI.Controllers
 {
+    [Route("api/HotelAPI")]
     [ApiController]
-    public class HotelAPIController :ControllerBase
+    public class HotelAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Hotel> GetHotels()
+        {
+            return new List<Hotel>
+            {
+                new Hotel{Id=1, Name="Pool View" },
+                new Hotel{Id=2, Name="Beach View"}
+            };
+        }
     }
 }
