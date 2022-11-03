5f0d254cc08af9c417c586f88b00e4487c4da55d
sdin <sdin@LAPTOP-DINESH>
2022-11-03 14:59:51 +0100

    Changing Logger with Dependency Injection - Serilog

--- Hotel_HotelAPI/Program.cs
+++ Hotel_HotelAPI/Program.cs
@@ -1,7 +1,13 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+    .WriteTo
+    .File("log/VillaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+builder.Host.UseSerilog();
 builder.Services.AddControllers(option=> {
     //option.ReturnHttpNotAcceptable = true;
     }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
