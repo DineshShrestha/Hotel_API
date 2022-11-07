a59ec077cf403becbce22ee5828725c132c73b27
sdin <sdin@LAPTOP-DINESH>
2022-11-07 14:54:06 +0100

    Added villa Repository Interface

---
+++ Hotel_HotelAPI/Repository/IRepository/IHotelRepository.cs
@@ -0,0 +1,19 @@
+﻿using Hotel_HotelAPI.Models;
+using System.Linq.Expressions;
+
+namespace Hotel_HotelAPI.Repository.IRepository
+{
+    public interface IHotelRepository
+    {
+        Task<List<Hotel>> GetAll(Expression<Func<Hotel>> filter = null);
+        Task<Hotel> Get(Expression<Func<Hotel>> filter = null, bool tracked=true);
+
+        Task<Hotel> Update(Hotel entity);
+
+        Task<Hotel> Remove(Hotel entity);
+
+        Task Create(Hotel entity);
+
+        Task Save();
+    }
+}
